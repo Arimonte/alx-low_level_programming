120c25b495208e646f9cd7b4bbb0d3ad9c6dffa6
Arimonte <arimonte2002@gmail.com>
2022-09-09 16:45:21 -0700

    oven

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@
  *
  */
 int main(void)
-{
+{	
 	int n;
 
 	srand(time(0));
@@ -19,14 +19,11 @@ int main(void)
 	{	
 		printf("%d is positive\n", n);						
 	}
-      		else if (n == 0)
-	{	
-		printf("%d is zero\n", n);
-				
-	}
-		
-  		else
-		 	printf("%d is negative\n", n);
-			
-		return (0);
- 	}
+	else if (n == 0)
+	{		
+		printf("%d is zero\n", n);		
+	}	
+	else
+		printf("%d is negative\n", n);	
+	return (0);
+}
