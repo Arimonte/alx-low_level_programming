907e41c285e2f374cfe50cd57e25f0a1fab4e035
Arimonte <arimonte2002@gmail.com>
2022-09-09 18:07:11 -0700

    neatly

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this print alphabet in lowercase
+ *
+ * Return: alway returns 0
+ *
+ */
+
+int main(void)
+
+{
+	char a;
+
+
+
+	for (a = 'a'; a <= 'z' ; a++)
+
+	putchar(a);
+
+	putchar('\n');
+
+	return (0);
+
+}
