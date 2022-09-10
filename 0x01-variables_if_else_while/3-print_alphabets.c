a8e2efac25b6a5cf803d44cd4a1a2a113047959f
Arimonte <arimonte2002@gmail.com>
2022-09-09 18:21:05 -0700

    elephant

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+* main - this print alphabet in lower and upperca
+*
+* Return: Always 0.
+*/
+
+int main(void)
+
+{
+
+	char a = 'a';
+
+	char b = 'A';
+
+
+
+	for (a = 'a'; a <= 'z'; a++)
+
+	{
+
+		putchar(a);
+
+	}
+
+	for (b = 'A'; b <= 'Z'; b++)
+
+	{
+
+		putchar(b);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
