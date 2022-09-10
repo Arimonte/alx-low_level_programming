a8db5f90dfeab7e0b0ad1e8c370262efb51482f3
Arimonte <arimonte2002@gmail.com>
2022-09-09 20:02:15 -0700

    daye

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - entry point
-
  *
-
  * Return: alway returns 0
-
  *
-
  */
 
 int main(void)
 
 {
-
 	int i;
-
 	int j;
-
 
 
 	for (i = 0; i <= 99; i++)
@@ -33,31 +23,21 @@ int main(void)
 		{
 
 			putchar(i / 10 + '0');
-
 			putchar(i % 10 + '0');
-
 			putchar(' ');
-
 			putchar(j / 10 + '0');
-
 			putchar(j % 10 + '0');
 
 			if (!(i == 98 && j == 99))
 
 			{
-
 				putchar(',');
-
 				putchar(' ');
 
 			}
-
 		}
-
 	}
 
 	putchar('\n');
-
 	return (0);
-
 }
