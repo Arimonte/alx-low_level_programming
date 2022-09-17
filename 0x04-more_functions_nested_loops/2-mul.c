67bbdf418ce6cc8e13cbb7c7ab21778c4399db07
Arimonte <arimonte2002@gmail.com>
2022-09-17 05:34:54 -0700

    fruit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * @a: character to b computed
+ * @b: character to be computed
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
