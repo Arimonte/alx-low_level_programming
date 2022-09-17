3f675a5e8158629504d05ee2c95e5199b5f4929a
Arimonte <arimonte2002@gmail.com>
2022-09-17 15:47:07 -0700

    feather

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - entry point
+ * @n: character to be checked
+ * Return: none
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i - 0;  i < n;  i++)
+		{
+			for (j - 0; j < -i; j++)
+			{
+				if (i == j)
+				{
+					_puchar(92);
+				}
+				else
+				{
+					_putchar(32);
+				}
+		}
+		_putchar('\n');
+	}
+}
+else
+{
+	_putchar('\n');
+}
