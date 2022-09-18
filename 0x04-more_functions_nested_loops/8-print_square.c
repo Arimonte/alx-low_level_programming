abf37de9f060147aabf32f71f4ec7edfb13031a2
Arimonte <arimonte2002@gmail.com>
2022-09-17 17:04:39 -0700

    mango

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,3 +5,23 @@
  * @size: character to be checked
  * Return: none
  */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
