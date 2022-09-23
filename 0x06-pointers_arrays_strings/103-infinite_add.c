62777df73bcfcd77216133efe2a9be53d953f90e
Arimonte <arimonte2002@gmail.com>
2022-09-22 17:47:23 -0700

    deal

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,26 +9,15 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 
 /**
-
  * add_strings - Adds the numbers stored in two strings.
-
  * @n1: The string containing the first number to be added.
-
  * @n2: The string containing the second number to be added.
-
  * @r: The buffer to store the result.
-
  * @r_index: The current index of the buffer.
-
  *
-
  * Return: If r can store the sum - a pointer to the result.
-
  *         If r cannot store the sum - 0.
-
  */
-
-
 
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 
@@ -37,11 +26,9 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	int num, tens = 0;
 
 
-
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 
 	{
-
 		num = (*n1 - '0') + (*n2 - '0');
 
 		num += tens;
@@ -51,7 +38,6 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		tens = num / 10;
 
 	}
-
 
 
 	for (; *n1; n1--, r_index--)
@@ -104,29 +90,16 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 
 }
 
-
-
 /**
-
  * infinite_add - Adds two numbers.
-
  * @n1: The first number to be added.
-
  * @n2: The second number to be added.
-
  * @r: The buffer to store the result.
-
  * @size_r: The buffer size.
-
  *
-
  * Return: If r can store the sum - a pointer to the result.
-
  *         If r cannot store the sum - 0.
-
  */
-
-
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
@@ -134,25 +107,17 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	int index, n1_len = 0, n2_len = 0;
 
-
-
 	for (index = 0; *(n1 + index); index++)
 
 		n1_len++;
-
-
 
 	for (index = 0; *(n2 + index); index++)
 
 		n2_len++;
 
-
-
 	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 
 		return (0);
-
-
 
 	n1 += n1_len - 1;
 
@@ -160,8 +125,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	*(r + size_r) = '\0';
 
-
-
 	return (add_strings(n1, n2, r, --size_r));
-
 }
