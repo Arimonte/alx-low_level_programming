1b018da5d4bb477efda1bda4973e1756d140f7fe
Arimonte <arimonte2002@gmail.com>
2022-09-27 11:52:51 -0700

    melody

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strchr - main function
+*@s: Pointer to the string
+*@c: Character to locate in the string
+*
+* Return: Pointer to first occurance of the c if it's there or NULL otherwise
+*/
+
+char *_strchr(char *s, char c)
+
+{
+
+	do {
+
+		if (*s == c)
+
+		{
+
+			return (s);
+
+		}
+
+	} while (*s++);
+
+	return (NULL);
+
+}
