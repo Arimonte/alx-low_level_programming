22322f7bb321f31fab97c324ced7b76ddd692151
Arimonte <arimonte2002@gmail.com>
2022-09-29 15:22:25 -0700

    miracle

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - prints a function that return the length of a stng
+* @s: pointer to string
+* Return: nothing
+*/
+
+int _strlen_recursion(char *s)
+
+{
+
+	if (*s == '\0')
+
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));
+
+}
