9a7279718132888b928df846dc7f6477348d9108
Arimonte <arimonte2002@gmail.com>
2022-10-03 07:08:32 -0700

    flawless

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+* @argc: Number of arguments passed
+* @argv: Vector of arguments strings
+*
+* Return: Always (0) Success
+*/
+int main(int argc, char **argv_attribute_((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
