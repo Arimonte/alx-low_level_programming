8b73cfc32eaa091b3f96ac4fa0f08d380ef99449
Arimonte <arimonte2002@gmail.com>
2022-10-10 12:12:55 -0700

    pomengranate

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,12 @@
 #define DOG_H
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+/**
  * struct dog - structure for dogs
  * @name: first member
  * @age: second member
@@ -12,13 +18,19 @@
 struct dog
 
 {
+
 	char *name;
 	float age;
 	char *owner;
+
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
 
 #endif
