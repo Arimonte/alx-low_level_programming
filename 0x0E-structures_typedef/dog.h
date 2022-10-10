f799962e7a7d4126699c563d1bacf5aadf106ff3
Arimonte <arimonte2002@gmail.com>
2022-10-10 12:10:11 -0700

    finder

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,12 +3,6 @@
 #define DOG_H
 
 /**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
-/**
  * struct dog - structure for dogs
  * @name: first member
  * @age: second member
@@ -25,7 +19,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
