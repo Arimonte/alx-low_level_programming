ea67f3f6d715a0ea3bcc865766825a88c0bd80d2
Arimonte <arimonte2002@gmail.com>
2022-10-12 14:24:01 -0700

    sandals

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,11 +6,8 @@
  * @f:function to execute
  *
  */
-
 void print_name(char *name, void (*f)(char *))
-
 {
-	if (name != NULL && f != NULL)
-
-		f(name);
+	if (name != NULL && f != NULL
+		f(name)
 }
