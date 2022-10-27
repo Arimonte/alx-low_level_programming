3271fe0a9dab92f9bbd93a2874fe3094acb6d15c
Arimonte <arimonte2002@gmail.com>
2022-10-27 14:22:55 -0700

    gazelle

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,13 +2,14 @@
 
 #define MAIN_H
 
-#include <stdlib.h> /* NULL */
-
-/* helper function */
+/*
+ * File: main.h
+ *
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x13-bit_manipulation directory.
+ */
 
 int _putchar(char c);
-
-/* function prototypes for all files */
 
 unsigned int binary_to_uint(const char *b);
 
@@ -23,5 +24,7 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 int get_endianness(void);
+
+
 
 #endif /* MAIN_H */
